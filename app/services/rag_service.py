034b75a886bd020dada0793f44fae7061a9b074d
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-09-24 19:39:49 -0500

    updated the llm service and added ollama

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,5 +1,5 @@
 import asyncio
-
+from app.services.llm_service import LLMService
 async def generate_answer(query: str) -> str:
     await asyncio.sleep(1) 
     return f"Answer: {query}"
@@ -9,7 +9,15 @@ class RAGService:
         pass
 
     async def answer(self, query: str) -> str:
-        answer = await generate_answer(query=query)
+        llm_service = LLMService()
+        # print(f"Test: {llm_service.call()}")
+        # answer = await generate_answer(query=query)
+        answer = await llm_service.a_call()
+        return answer
+    
+    async def test_llm(self, query: str) -> str:
+        llm_service = LLMService()
+        answer = await llm_service.a_call()
         return answer
 
     
